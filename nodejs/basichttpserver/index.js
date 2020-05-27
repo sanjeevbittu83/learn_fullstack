c2b8a15cecdd39e130db23682420f542a1e9022d
sanjeevbittu83 <sanjeevbittu83@gmail.com>
2020-05-27 10:47:43 +0530

    server run with extranal  file

--- nodejs/basichttpserver/index.js
+++ nodejs/basichttpserver/index.js
@@ -1,16 +1,23 @@
 const http= require('http');
 const port=8001;
+const fs=require('fs');
+
 function requestHandler(request,response){
     console.log(request.url);
+    response.writeHead(200,{'content-type':'text/html'})
 
-
-    response.end('gotcha!');
+    fs.readFile('./index.html',function(err,data){
+        if(err){
+            console.log('error',err);
+            return response.end('<h1>ERROR</h1>') ;
+        }
+        return response.end(data);
+    });
 }
-const server=http.createServer(requestHandler);
+const server = http.createServer(requestHandler);
 server.listen(port,function(err){
     if(err){
-        console.log(err);
-        return ;
+        return;
     }
-    console.log("server is running on port",port);
-});
+    console.log("Server on port ", port);
+})
