4c6d8c139f2f9977cb24fc9ede7dc1f0a7d47240
sanjeevbittu83 <sanjeevbittu83@gmail.com>
2020-06-03 15:40:29 +0530

    basic contact list1

--- nodejs/contact_list_express/index.js
+++ nodejs/contact_list_express/index.js
@@ -1,11 +1,33 @@
-const express=require('express');
-const port=8001;
-const app=express();
+const express = require('express');
+const path = require('path');
+const port = 8001;
+const app = express();
+app.set('view engine', 'ejs');
+app.set('views', path.join( __dirname, 'views'));
 
-
-app.listen(port,function(err){
-    if(err){
-        console.log('error is running',err);
+var contactlist=[
+    {
+        name:"sanjeev",
+        phone:"11111"
+    },
+    {
+        name:"bittu",
+        phone:"12121"
     }
-        console.log('its running on ',port);
+]
+
+app.get('/', function (req, res) {
+    return res.render(
+        'home', {
+            title: "contact list",
+            contact_list:contactlist
+        }
+    );
+});
+
+app.listen(port, function (err) {
+    if (err) {
+        console.log('error is running', err);
+    }
+    console.log('its running on ', port);
 });
