2a277f9c186c91f2420913fa376945dce3fbbf2e
sanjeevbittu83 <sanjeevbittu83@gmail.com>
2020-06-07 23:03:26 +0530

    database connection

--- nodejs/contact_list_express/index.js
+++ nodejs/contact_list_express/index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const path = require('path');
 const port = 8001;
+const db=require('./config/mongoose')
 const app = express();
 app.set('view engine', 'ejs');
 app.set('views', path.join( __dirname, 'views'));
