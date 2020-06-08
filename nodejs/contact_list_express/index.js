e2a16a6cde26c43c1fd9411a569936e9d1093486
sanjeevbittu83 <sanjeevbittu83@gmail.com>
2020-06-08 10:46:34 +0530

    delete contact

--- nodejs/contact_list_express/index.js
+++ nodejs/contact_list_express/index.js
@@ -21,12 +21,22 @@ var contactlist=[
         phone:"12121"
     }
 ]
-
+//FETCHING THE CONTACT LIST;
 app.get('/', function (req, res) {
-    return res.render(
-        'home', {
-            title: "contact list",
-            contact_list:contactlist
+    Contact.find({},function(err,contacts){
+if(err){
+    console.log('error in fetching contacts in database');
+    return;
+}
+return res.render(
+    'home', {
+        title: "contact list",
+        contact_list:contacts
+    });
+    // return res.render(
+    //     'home', {
+    //         title: "contact list",
+    //         contact_list:contactlist
         }
     );
 });
@@ -45,16 +55,40 @@ app.post('/create-contact',function(req,res){
 //       phone:req.body.phone
 //   });
  
-contactlist.push(req.body);
- return res.redirect('/');
+
+
+// contactlist.push(req.body);
+//  return res.redirect('/');
+
+//insert into database
+Contact.create({
+    name:req.body.name,
+    phone:req.body.phone
+},function(err,newContact){
+    if(err){
+        console.log('error to creating contact list');
+        return;
+    }
+    console.log('****',newContact);
+    return res.redirect('back');
+});
 }); 
+//DELETE CONTACT
  app.get('/delete-contact',function(req,res){
-     let phone=req.query.phone;
- let contactIndex=contactlist.findIndex(contact=>contact.phone==phone);
- if(contactIndex!=-1){
-     contactlist.splice(contactIndex,1);
- }
- return res.redirect('back');
+     //GET THE ID FROM QUERY IN THE URL;
+     let id=req.query.id;
+     //FIND THE CONTACT IN DATABASE USIND ID AND DELETE
+Contact.findByIdAndDelete(id,function(err){
+if(err){
+    console.log('error to delete contact');
+    return;
+}
+
+
+return res.redirect('back');
+
+}); 
+
  });
 
 app.listen(port, function (err) {
